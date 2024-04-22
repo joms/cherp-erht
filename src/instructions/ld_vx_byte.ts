542be0895cacb931c01843c0d8a47a9f60431333
Jo Emil Holen <jo-emil@outlook.com>
2024-04-22 20:54:25 +0200

    🧅

--- src/instructions/ld_vx_byte.ts
+++ src/instructions/ld_vx_byte.ts
@@ -4,6 +4,5 @@ import { getArguments } from "./getArguments";
 export const LD_VX_BYTE = (opcode: number, cpu: CPU) => {
   const [x] = getArguments(opcode);
 
-  cpu.registers[x] = opcode && 0xff;
-  cpu.incrementPc();
+  cpu.registers[x] = opcode & 0xff;
 };
